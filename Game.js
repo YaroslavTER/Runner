5fd71b8afba8c5b04228811a6a6b666c28d031c6
Yaroslav <yaroslavtereshchuk@gmail.com>
2017-01-13 20:51:27 +0200

    to fix

--- Game.js
+++ Game.js
@@ -5,12 +5,13 @@ var height = canvas.clientHeight, width = canvas.clientWidth
 const GRAY = "#474747"
 const DARK_GRAY = "#303030"
 
-var userBlock = {x: 0, y: 0, height: 30}
+var userBlock = {x: 0, y: 0, height: 30, onSurface: false}
 var moveStep = 1, jump = false
 var time = 1
 var middle = width/2-userBlock.height
 
 var mainGameCycle
+var isRun = false
 var platform = {height: 5, width: width}
 var enemyes = []
 
@@ -30,18 +31,27 @@ function RunGame(){
 }
 
 function Jump(){
-    if(jump){
+    CheckSurface()
+    if(userBlock.onSurface){
         userBlock.y += moveStep
-        jump = userBlock.y < userBlock.height + userBlock.height/2 ? true : false
-    } else if(!jump && userBlock.y > 0)
+        userBlock.onSurface = userBlock.y < userBlock.height +
+                                            userBlock.height/2 ? true : false
+    } else if(!userBlock.onSurface && userBlock.y > 0)
         userBlock.y -= moveStep
+}
+
+function CheckSurface(){
+    if(userBlock.y == 0)
+        userBlock.onSurface = true
+    else userBlobck.onSurface = false
 }
 
 function Draw(){
     ctx.clearRect(0, 0, width, height)
 
     ctx.fillStyle = GRAY
-    ctx.fillRect(userBlock.x, height/2-userBlock.y, userBlock.height, userBlock.height)
+    ctx.fillRect(userBlock.x, height/2-userBlock.y,
+                 userBlock.height, userBlock.height)
 
     ctx.fillStyle = DARK_GRAY
     ctx.fillRect(0, height/2+userBlock.height,
@@ -52,10 +62,14 @@ function RunEnemies(){
 
 }
 
+function Game(){ RunGame() ? isRun : Draw() }
+
+Game()
+
 document.addEventListener('keydown', function(event) {
     var keycode = (event.keyCode ? event.keyCode : event.which)
+    if(event.which == 13)
+        isRun = true
     if(event.which == 32 && userBlock.y == 0)
         jump = true
 })
-
-RunGame()
