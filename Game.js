d4676ccc75c69972f8972601eebf6ab973713851
Yaroslav <yaroslavtereshchuk@gmail.com>
2017-01-13 21:47:49 +0200

    qwerty

--- Game.js
+++ Game.js
@@ -5,14 +5,14 @@ var height = canvas.clientHeight, width = canvas.clientWidth
 const GRAY = "#474747"
 const DARK_GRAY = "#303030"
 
-var userBlock = {x: 0, y: 0, height: 30, onSurface: false}
+var userBlock = {x: 0, y: 0, height: 30}
 var moveStep = 1, jump = false
 var time = 1
 var middle = width/2-userBlock.height
 
 var mainGameCycle
-var isRun = false
-var platform = {height: 5, width: width}
+var run = false
+var platform = {height: 2.5, width: width}
 var enemyes = []
 
 function RunGame(){
@@ -31,45 +31,30 @@ function RunGame(){
 }
 
 function Jump(){
-    CheckSurface()
-    if(userBlock.onSurface){
-        userBlock.y += moveStep
-        userBlock.onSurface = userBlock.y < userBlock.height +
-                                            userBlock.height/2 ? true : false
-    } else if(!userBlock.onSurface && userBlock.y > 0)
-        userBlock.y -= moveStep
-}
-
-function CheckSurface(){
-    if(userBlock.y == 0)
-        userBlock.onSurface = true
-    else userBlobck.onSurface = false
+    if(jump){
+        userBlock.y += moveStep*1.5
+        jump = userBlock.y < userBlock.height*2 ? true : false
+    } else if(!jump && userBlock.y > 0)
+        userBlock.y -= moveStep*1.5
 }
 
 function Draw(){
     ctx.clearRect(0, 0, width, height)
 
     ctx.fillStyle = GRAY
-    ctx.fillRect(userBlock.x, height/2-userBlock.y,
-                 userBlock.height, userBlock.height)
+    ctx.fillRect(userBlock.x, height/2-userBlock.y, userBlock.height, userBlock.height)
 
     ctx.fillStyle = DARK_GRAY
     ctx.fillRect(0, height/2+userBlock.height,
                  platform.width, platform.height)
 }
 
-function RunEnemies(){
+function RunEnemies(){}
 
-}
-
-function Game(){ RunGame() ? isRun : Draw() }
-
-Game()
+RunGame()
 
 document.addEventListener('keydown', function(event) {
     var keycode = (event.keyCode ? event.keyCode : event.which)
-    if(event.which == 13)
-        isRun = true
     if(event.which == 32 && userBlock.y == 0)
         jump = true
 })
